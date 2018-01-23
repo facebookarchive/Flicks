6b536eabcf7750897309307445f7fce2dd83e664
Mac Mason <mac@kingseye.net>
2018-01-22 20:03:46 -0800

    IWYU: #include <ratio>

--- flicks.h
+++ flicks.h
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2017-present, Facebook, Inc.
  * All rights reserved.
- * 
+ *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
@@ -11,6 +11,7 @@
 #include <chrono>
 #include <cstdint>
 #include <functional>
+#include <ratio>
 
 namespace util {
 
